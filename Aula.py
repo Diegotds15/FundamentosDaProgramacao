7ec98d96101b7398bb52714e4cb1f8e8d13bce7d
Diegotds15 <tonamotinho@gmail.com>
2024-10-30 11:22:46 -0300

    Att.

--- Aula.py
+++ Aula.py
@@ -1,7 +1,30 @@
-perg1 = input('Está chuvendo? S - Sim/N - Não: ').upper()
-perg2 = input('Tem Guarda-chuva? S - Sim/N - Não: ').upper()
+'''def soma(a, b):
+    return a + b
 
-if (perg1 == 'S' and perg2 == 'S') or (perg2 == 'S') or (perg1 == 'N' and perg2 == 'N'):
-    print('pode sair tranquilo!')
-else:
-    print('não pode sair tranquilo!')
+def main():
+    resutado = soma(3, 7)
+    print(f"o resultado é {resutado}")
+
+main()'''
+
+'''x = 10  # Variável global
+
+def minha_funcao():
+    print(x)  # Imprime 5
+
+minha_funcao()
+print(x)  # Imprime 10'''
+
+'''total = 0
+
+for i in range(5):
+    total += i
+
+print(total)'''
+
+preco = int(input("Qual o preço de seu produto?: "))
+desconto = int(input("Qual o porcentual do desconto?: "))
+
+valor_descontado = (preco / 100) * desconto
+resultado = preco - valor_descontado
+print(f"O resultado é R${resultado}")
